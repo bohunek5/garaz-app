f31f4f0d23858275e2cce290764bd349d38cd492
Karol Bohdanowicz <karolbohdanowicz@mac.home>
2026-02-02 21:34:05 +0100

    fix: add basePath and CNAME for sternicy.com deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: "/garaz-app",
+    assetPrefix: "/garaz-app",
     images: {
         unoptimized: true,
     },
